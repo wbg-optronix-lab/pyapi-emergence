2e5e70e042110e0f400406e067073ea2b9c56ac1
Neil Newman <nnewman2@albany.edu>
2015-02-18 17:32:35 -0500

    Added relative tree API

--- emergence/__init__.py
+++ emergence/__init__.py
@@ -161,6 +161,15 @@ class Emergence(object):
         """
         return self.get('/sample/{}/node/tree/'.format(uuid))
 
+    def get_sample_tree_relative(self, uuid):
+        """
+        Show details for a specific sample specified by uuid.
+        Retrieves tree view.
+
+        :param uuid: UUID of sample
+        """
+        return self.get('/sample/{}/node/tree/relative/'.format(uuid))
+
     def get_process_list(self):
         """
         Returns a list of processes and generic details
@@ -189,7 +198,7 @@ class Emergence(object):
 
         :param uuid: UUID of process node.
         """
-        return self.get('/sample/by_process/{}'.format(uuid))
+        return self.get('/sample/{}/node/tree/by_process/'.format(uuid))
 
     ############
     # D180 API #
